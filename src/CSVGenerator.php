cd400f6b53318aa05fa297a7a39cd51573c701f9
Romain <kerkroy@gmail.com>
2020-12-14 20:47:54 +0100

    first commit

---
+++ src/CSVGenerator.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * (c) Romain <kerkroy@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace kerkroy\TableGenerator;
+
+class CSVGenerator
+{
+
+    public function generate(array $table): string
+    {
+        return 'table generated';
+    }
+
+}
